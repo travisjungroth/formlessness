0365ed232adbe57d4f7808b0cfa1cfbf52a585ca
Travis Jungroth <jungroth@gmail.com>
2022-05-25 17:22:02 -0700

    autoformat

--- dodo.py
+++ dodo.py
@@ -24,6 +24,25 @@ def task_test():
     }
 
 
+def task_autoformat():
+    return {"actions": [_autoformat]}
+
+
+def _autoformat():
+    message = "autoformat"
+    out = subprocess.run(
+        ["git", "log", "--format=%s", "-3"], capture_output=True, text=True
+    ).stdout
+    if out == f"{message}\n" * 3:
+        print("Three times, enough already.")
+        exit(1)
+
+    subprocess.run(
+        ["doit", "format_last"]
+    )
+    subprocess.run(["git", "commit", "-am", message])
+
+
 def _changed_files(ref: str):
     out = subprocess.run(
         ["git", "diff", ref, "--name-only", "--diff-filter=d"],
@@ -36,5 +55,3 @@ def _changed_files(ref: str):
 def _shed(files):
     files = [f for f in files if f[-3:] in (".py", ".md")]
     return {"actions": [f"shed {' '.join(files)} --py39-plus"], "file_dep": files}
-
-def xys(): pass
