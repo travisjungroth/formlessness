f5c8987e74976b35d629789f27e53d79cc540fc6
Travis Jungroth <jungroth@gmail.com>
2022-06-08 22:58:52 -0700

    Change key_and_label

--- formlessness/utils.py
+++ formlessness/utils.py
@@ -4,12 +4,20 @@ from typing import Iterable
 def key_and_label(key: str, label: str) -> tuple[str, str]:
     """
     If one of key or label is missing, generate it from the other.
+
+    >>> key_and_label("foo_bar", "Foo Bar")
+    ('foo_bar', 'Foo Bar')
+    >>> key_and_label("foo_bar", "")
+    ('foo_bar', 'Foo Bar')
+    >>> key_and_label("", "Foo Bar")
+    ('foo_bar', 'Foo Bar')
+    >>> key_and_label("", "Foo Bar!")
+    ('foo_bar', 'Foo Bar!')
     """
-    # TODO: Add a doctest
     if key and label:
         return key, label
     if key:
-        return key, key
+        return key, key.replace('_', ' ').title()
     if label:
         key = "".join(
             [c for c in label.lower().replace(" ", "_") if c.isalnum() or c == "_"]
