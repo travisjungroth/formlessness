2ec1224cb7106a8bd15bdde8f2998567a1198088
Travis Jungroth <jungroth@gmail.com>
2022-06-23 11:40:47 -0700

    Reduce early simplification (#165)

--- formlessness/constraints.py
+++ formlessness/constraints.py
@@ -94,7 +94,7 @@ class Constraint(Generic[T], ABC):
         >>> float_over_10.satisfied_by(11)
         False
         """
-        return And(self, other).simplify()
+        return And(self, other)
 
     def __or__(self, other: Constraint) -> Constraint:
         """
@@ -102,7 +102,7 @@ class Constraint(Generic[T], ABC):
         >>> big_or_small.satisfied_by(5)
         True
         """
-        return Or(self, other).simplify()
+        return Or(self, other)
 
     def __invert__(self) -> Constraint:
         """
@@ -398,7 +398,7 @@ class Or(Constraint[T]):
         return any(self.constraints)
 
     def __invert__(self) -> Constraint:
-        return And(*[~c for c in self.constraints]).simplify()
+        return And(*[~c for c in self.constraints])
 
     def simplify(self) -> Constraint:
         """
@@ -455,7 +455,7 @@ class And(Constraint[T]):
         return all(self.constraints)
 
     def __invert__(self) -> Constraint:
-        return Or(*[~c for c in self.constraints]).simplify()
+        return Or(*[~c for c in self.constraints])
 
     def simplify(self) -> Constraint:
         """
@@ -531,7 +531,6 @@ class If(Constraint[T]):
     p: Constraint
     q: Constraint
     message: str = ""
-    simplified: ClassVar[bool] = False
 
     def satisfied_by(self, value: T) -> bool:
         """
