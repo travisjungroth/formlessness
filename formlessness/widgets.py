5cef30a6991be79683078cce0b3edfba3e730d9f
Natasha Vinik <99206257+natashavinik@users.noreply.github.com>
2022-06-29 10:52:22 -0700

    Add text_area widget (#176)
    Co-authored-by: Natasha Vinik <natasha.n.vinik@gmail.com>

--- formlessness/widgets.py
+++ formlessness/widgets.py
@@ -7,5 +7,6 @@ class Widget(TypedDict):
 
 
 text: Final[Widget] = Widget(type="text")
+text_area: Final[Widget] = Widget(type="text_area")
 date_picker: Final[Widget] = Widget(type="date_picker")
 checkbox: Final[Widget] = Widget(type="checkbox")
