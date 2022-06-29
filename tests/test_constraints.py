dc88f848d3553b7ac169a23cd7621df792bfaaac
Travis Jungroth <jungroth@gmail.com>
2022-06-29 11:25:25 -0700

    Move Regex tests off docstrings
    These were causing warnings. Moving them out to get things passing, then may move back.

--- tests/test_constraints.py
+++ tests/test_constraints.py
@@ -1,6 +1,15 @@
 from formlessness.constraints import GE
 from formlessness.constraints import Comparison
+from formlessness.constraints import Regex
 from formlessness.constraints import to_json
+
+
+def test_regex():
+    constraint = Regex(r"\w+")
+    assert constraint.satisfied_by("snake_case")
+    assert not constraint.satisfied_by("abc!")
+    assert str(constraint) == r"Must match regex \w+"
+    assert not Regex(r"\w").satisfied_by("snake_case")
 
 
 def test_json_ge():
