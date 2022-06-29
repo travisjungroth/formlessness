953410baaa1f2bfd1d8ee73d705c4cdd1f990910
Natasha Vinik <natashavinik@Natashas-MacBook-Pro-2.local>
2022-06-29 10:39:50 -0700

    Add text_area widget

--- formlessness/widgets.py
+++ formlessness/widgets.py
@@ -1,11 +1,10 @@
 from typing import Final
 from typing import TypedDict
-
-
 class Widget(TypedDict):
     type: str
 
 
 text: Final[Widget] = Widget(type="text")
+text_area: Final[Widget] = Widget(type="text_area")
 date_picker: Final[Widget] = Widget(type="date_picker")
 checkbox: Final[Widget] = Widget(type="checkbox")
